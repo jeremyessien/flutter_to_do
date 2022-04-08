63ee596c10ae823c30f3e112d7acbdf6a6d50f70
Jeremiah Ekanem <HunckleJeremy@users.noreply.github.com>
2022-04-08 21:29:33 +0100

    Update tasks_screens.dart

--- lib/screens/tasks_screens.dart
+++ lib/screens/tasks_screens.dart
@@ -12,72 +12,73 @@ class TaskScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.lightBlueAccent,
         child: Icon(Icons.add),
-        onPressed: (){},
+        onPressed: () {},
       ),
       body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+      Container(
+      padding: EdgeInsets.only(top: 60, left: 30, right: 30),
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            padding: EdgeInsets.only(top: 60, left: 30, right: 30),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CircleAvatar(
-                  backgroundColor: Colors.white,
-                  radius: 30.0,
-                  child: const Icon(
-                    Icons.list,
-                    size: 30.0,
-                    color: Colors.lightBlueAccent,
-                  ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                const Text(
-                  'To Do',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 50.0,
-                      fontWeight: FontWeight.w700),
-                ),
-                Text(
-                  '12 Tasks',
-                  style: TextStyle(color: Colors.white, fontSize: 18),
-                ),
-              ],
+          CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 30.0,
+            child: const Icon(
+              Icons.list,
+              size: 30.0,
+              color: Colors.lightBlueAccent,
             ),
           ),
-          SizedBox(height: 20,),
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
+          SizedBox(
+            height: 10.0,
+          ),
+          const Text(
+            'To Do',
+            style: TextStyle(
                 color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                ),
-              ),
-              child: ListView(
-                children: [
-                  ListTile(
-                    leading: Text('Buy milk'),
-                    trailing: Icon(Icons.check_box_outline_blank),
-                  ),
-                  ListTile(
-                    leading: Text('Buy eggs'),
-                    trailing: Icon(Icons.check_box_outline_blank),
-                  ),
-                  ListTile(
-                    leading: Text('Buy chocolate'),
-                    trailing: Icon(Icons.check_box_outline_blank),
-                  )
-                ],
-              ),
-            ),
+                fontSize: 50.0,
+                fontWeight: FontWeight.w700),
+          ),
+          Text(
+            '12 Tasks',
+            style: TextStyle(color: Colors.white, fontSize: 18),
           ),
         ],
       ),
+    ),
+    SizedBox(
+    height: 20,
+    ),
+    Expanded(
+    child: Container(
+    decoration: BoxDecoration(
+    color: Colors.white,
+    borderRadius: BorderRadius.only(
+    topLeft: Radius.circular(20),
+    topRight: Radius.circular(20),
+    ),
+    ),
+    child: ListView(
+    children: [
+    ListTile(
+    leading: Text('Buy milk'),
+    trailing: Checkbox(value: false, , onChanged: (bool? value) {},)
+    ),
+    ListTile(
+    leading: Text('Buy eggs'),
+    trailing: Checkbox(value: false, , onChanged: (bool? value) {},), ),
+    ListTile(
+    leading: Text('Buy chocolate'),
+    trailing: Checkbox(value: false, , onChanged: (bool? value) {},), ),
+    )
+    ],
+    ),
+    ),
+    ),
+    ],
+    ),
     );
-  }
+    }
 }
