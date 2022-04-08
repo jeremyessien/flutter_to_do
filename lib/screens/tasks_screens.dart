820e7719b04ade0e1c67e906cfd47cff22d35c43
Jeremiah Ekanem <HunckleJeremy@users.noreply.github.com>
2022-04-08 21:33:41 +0100

    Update tasks_screens.dart

--- lib/screens/tasks_screens.dart
+++ lib/screens/tasks_screens.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_to_do/component/components.dart';
 
 class TaskScreen extends StatelessWidget {
   const TaskScreen({Key? key}) : super(key: key);
@@ -15,70 +16,57 @@ class TaskScreen extends StatelessWidget {
         onPressed: () {},
       ),
       body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-      Container(
-      padding: EdgeInsets.only(top: 60, left: 30, right: 30),
-      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          CircleAvatar(
-            backgroundColor: Colors.white,
-            radius: 30.0,
-            child: const Icon(
-              Icons.list,
-              size: 30.0,
-              color: Colors.lightBlueAccent,
+          Container(
+            padding: EdgeInsets.only(top: 60, left: 30, right: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: const Icon(
+                    Icons.list,
+                    size: 30.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                const Text(
+                  'To Do',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50.0,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ],
             ),
           ),
           SizedBox(
-            height: 10.0,
+            height: 20,
           ),
-          const Text(
-            'To Do',
-            style: TextStyle(
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: BoxDecoration(
                 color: Colors.white,
-                fontSize: 50.0,
-                fontWeight: FontWeight.w700),
-          ),
-          Text(
-            '12 Tasks',
-            style: TextStyle(color: Colors.white, fontSize: 18),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: TaskList(),
+            ),
           ),
         ],
       ),
-    ),
-    SizedBox(
-    height: 20,
-    ),
-    Expanded(
-    child: Container(
-    decoration: BoxDecoration(
-    color: Colors.white,
-    borderRadius: BorderRadius.only(
-    topLeft: Radius.circular(20),
-    topRight: Radius.circular(20),
-    ),
-    ),
-    child: ListView(
-    children: [
-    ListTile(
-    leading: Text('Buy milk'),
-    trailing: Checkbox(value: false, , onChanged: (bool? value) {},)
-    ),
-    ListTile(
-    leading: Text('Buy eggs'),
-    trailing: Checkbox(value: false, , onChanged: (bool? value) {},), ),
-    ListTile(
-    leading: Text('Buy chocolate'),
-    trailing: Checkbox(value: false, , onChanged: (bool? value) {},), ),
-    )
-    ],
-    ),
-    ),
-    ),
-    ],
-    ),
     );
-    }
+  }
 }
